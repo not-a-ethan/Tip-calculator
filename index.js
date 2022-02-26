dcc07640f10001bd286e038857a92d039cdcc9ba
Ethan <92686703+ethan-master-coding@users.noreply.github.com>
2022-02-26 13:46:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function tip(Stotal, tipPercent) {
-    percent = tipPercent + 1
+    percent = tipPercent * .01
 
-    total = percent * Stotal
+    amountOfTip = percent * Stotal
+    total = amountOfTip + Stotal
 
-    console.log(`Your total is ${total}`)
+    console.log(`Your total is ${total}. And you are tipping ${amountOfTip}`)
 }
